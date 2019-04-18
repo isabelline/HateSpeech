84b5a851dce1237ea5fe725099dc931d7fbb1238
isabelline <ehfalskfl1@snu.ac.kr>
2019-04-19 06:10:37 +0900

    Update prepare_input.py

--- prepare_input.py
+++ prepare_input.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.utils import to_categorical
 from ekphrasis.classes.preprocessor import TextPreProcessor 
 from ekphrasis.classes.tokenizer import SocialTokenizer 
 from ekphrasis.dicts.emoticons import emoticons
@@ -11,7 +12,7 @@ import pickle as pkl
 
 
 def check_extension(args):
-	if args.train[-3:] == "tsv":
+	if args['train'][-3:] == "tsv":
 		return "\t"
 	else:
 		return ","
@@ -24,11 +25,11 @@ def read_file(args, data):
 		df = pd.read_csv(file, sep=sep, encoding="utf-8")
 		data[key]['text'] = list(df.text)
 		try:
-			data[key]['label'] = list(df.label)
-			data[key]['y'] = to_categorical(np.asarray(list(df.label)))
+			data[key]['label'] = list(df.HS)
+			data[key]['y'] = to_categorical(np.asarray(list(df.HS)))
 		except:
 			print("No Labels in " + key)
-		text_all += df.text		
+		text_all += list(df.text)		
 	return text_all
 
 
@@ -37,31 +38,32 @@ def clean_then_tokenize_text(data):
 	for key in data:
 		text = data[key]
 		a= []
-		for line in text
-			if true
-				temp  =" ".join( text_to_word_sequence(line) ) 
+		temp = ""
+		for line in text:
+			if True:
+				temp=" ".join( text_to_word_sequence(line) ) 
 				a.append(temp)
 		data[key]['cln_text'] = a
 		text_all +=a
 	return text_all
 
 
-def save_clean_file(data):
+def save_clean_file(args, data):
 	for key in data:
-	text = data[key]['cln_text']
-	with open(os.path.join(args['data_dir'], key+".clean.txt"), 'w') as f:
-		for line in text:
-			f.write(line+"\n")
+		text = data[key]['cln_text']
+		with open(os.path.join(args['data_dir'], key+".clean.txt"), 'w') as f:
+			for line in text:
+				f.write(line+"\n")
 
 def get_text(args, data):
 	text_all = read_file(args, data)
 	clean_text_all = clean_then_tokenize_text(data)
-	save_clean_file(data)
+	save_clean_file(args, data)
 
 	return clean_text_all
 
 def get_vocab(text_all):
-	tokenizer = Tokenizer()     
+	tokenizer = Tokenizer()
 	tokenizer.fit_on_texts(text_all)
 	word_idx = tokenizer.word_index
 	return word_idx
@@ -74,12 +76,12 @@ def add_special_vocab(word_dict):
 
 def save_vocab(args, word_dict):
 	print("----------------save vocab----------------")
-	with open(os.path.join(args.data_dir, "vocab.txt"), 'w') as f:
+	with open(os.path.join(args['data_dir'], "vocab.pkl"), 'wb') as f:
 		pkl.dump(word_dict,f)
 
 
 def prepare_vocab(args, data):
-	clean_text_all = get_text(args)
+	clean_text_all = get_text(args,data)
 	word_dict = get_vocab(clean_text_all)
 	add_special_vocab(word_dict)	
 	save_vocab(args, word_dict)
@@ -87,7 +89,7 @@ def prepare_vocab(args, data):
 
 def load_vocab(args):
 	print("---------------load vocab--------------")
-	with open(args.data_dir + "vocab.txt", 'r') as f:
+	with open(os.path.join(args['data_dir'] ,"vocab.pkl"), 'rb') as f:
 		word_dict = pkl.load(f)
 	return word_dict
 
@@ -95,27 +97,27 @@ def load_vocab(args):
 def prepare_X(args, data):
 	word_dict = load_vocab(args)
 	for key in data:
-		x =  np.zeros((len(data[key]['text'], args['max_len'])))
+		x =np.zeros((len(data[key]['text']), int(args['max_len'])))
 		text = data[key]['cln_text']
-		for i, line in x.shape[0]:
+		for i, line in enumerate(text):
 			tokens = line.split()
-			for j in x.shape[1]:
-				if i >= len(token):
+			for j in range(x.shape[1]):
+				if i >= len(tokens):
 					continue
 				else:
-					x[i][j] = word_dict[token[i]]
+					x[i][j] = word_dict[tokens[i]]
 		data[key]['x'] = x
 
-def prepare_Y():
+def prepare_Y(args, data):
 	print("Already Done!")
 
-def to_numpy(data):
+def to_numpy(args, data):
 	print("----------data to numpy-----------")
 	for key in data:
 		np.save(os.path.join(args['data_dir'], key+".x"),data[key]['x'])
 	try:
 		np.save(os.path.join(args['data_dir'], key+".y"), data[key]['y'])
-	except: 
+	except:
 		print("No labels for " + key)
 
 def main():
@@ -126,26 +128,33 @@ def main():
 	parser.add_argument("--test")
 	parser.add_argument("max_len")
 	parser.add_argument("class_num")
+	parser.add_argument("--glove")
+	parser.add_argument("class_num")
+
 	parser.add_argument("--max_vocab", default = 30000)
 	args = parser.parse_args()
+	args = vars(args)
+	print("---------------parameters----------------")
+	print(args)
 
 	data = dict()
-	if train:
-		if true:
+	if args['train']:
+		if True:
 			data['train'] = dict()
 
-	if dev: 
-		if true:
+	if args['dev']:
+		if True:
 			data['dev'] = dict()
 
-	if test: 
-		if true:
+	if args['test']:
+		if True:
 			data['test'] = dict()
 
 	prepare_vocab(args, data)
-	prepare_X()
-	prepare_Y()
-	to_numpy(data)
+	prepare_X(args, data)
+	prepare_Y(args, data)
+	to_numpy(args, data)
+	prepare_glove(args, data)
 
 
 
